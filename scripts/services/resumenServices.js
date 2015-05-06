3afadefe732a68a7532999de2ec8049acdb33ac9
Vero <veroartola@gmail.com>
2015-05-06 14:41:19 -0300

    services con canceller pending request
    a cada service se le agrega un canceller pending request

--- scripts/services/resumenServices.js
+++ scripts/services/resumenServices.js
@@ -1,14 +1,22 @@
-app.service('resumenService', ['$http', function ($http) {
+app.service('resumenService', ['$http','$q','pendingRequests', function ($http,$q,pendingRequests) {
 	return {
 		resumen:function(d, h) {
-			return $http({
+			var canceller = $q.defer();
+			pendingRequests.add({
+				url: dir_api + '/resumen/index',
+				canceller: canceller
+			});
+			var promise = $http({
 				method: 'POST',
 	            url: dir_api + '/resumen/index',
 	            data: $.param({desde:d,hasta:h}),
 	            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-			})
+			});
+	            promise.finally(function() {
+      				pendingRequests.remove(url);
+    			});
+				return promise;
         },
-
 		detalle:function(d, h, fp) {
 			return $http({
 				method: 'POST',
--- scripts/services/ventasServices.js
+++ scripts/services/ventasServices.js
@@ -1,18 +1,24 @@
 
-app.service('ventasService', ['$http', function ($http) {
+app.service('ventasService', ['$http','$q','pendingRequests', function ($http,$q,pendingRequests) {
         return {
             ventas:function(d, h, cd, p) {
-	           return $http({
+	           var canceller = $q.defer();
+				pendingRequests.add({
+					url: dir_api + '/ventas/index',
+					canceller: canceller
+				});
+				var promise = $http({
 	            	method: 'POST',
 	            	url: dir_api + '/ventas/index',
 	            	data: $.param({desde:d,hasta:h,conDeuda:cd,pag:p}),
 	                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-	            })
+	            });
+	            promise.finally(function() {
+      				pendingRequests.remove(url);
+    			});
+				return promise;
             },
-            
-            /******************************
-            ADDVENTA
-            ******************************/
+            //ADDVENTA *********************************************************
             addVenta:function (pedido) {
 	            return $http({
 	            	method: 'POST',
@@ -21,65 +27,40 @@ app.service('ventasService', ['$http', function ($http) {
 	                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 	            })
             },
-            
-                         
-            /******************************
-            EDITVENTA
-            ******************************/
+            //EDITVENTA *********************************************************
             editVenta: function(venta){ 
-	            
 	            return $http({
 	            	method: 'PUT',
 	            	url: dir_api + '/ventas/update',
 	            	data: $.param(venta),
 	                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 	            });
-	            
-            },
-            
-                        
-            /******************************
-            DELETEVENTA
-            ******************************/
+	        },
+            //DELETEVENTA *********************************************************
             deleteVenta:function (id) { 
 	            return $http({
 	            	method: 'DELETE',
 	            	url: dir_api + '/ventas/'+id+'/delete',
 	                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 	            });
-	            
-            }, 
-            
-            
-            /******************************
-            PAGOS DE LA VENTA
-            ******************************/
+	        }, 
+            //PAGOS DE LA VENTA *********************************************************
             pagosVenta:function (id) { 
-	                    
 	            return $http({
 	            	method: 'GET',
 	            	url: dir_api + '/ventas/'+id+'/pagos',
 	                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 	            });
-	            
-            },
-
-			/******************************
-            DEVOLUCIONES DE LA VENTA
-            ******************************/
+	        },
+			//DEVOLUCIONES DE LA VENTA *********************************************************
             devolucionesVenta:function (id) { 
-	                    
 	            return $http({
 	            	method: 'GET',
 	            	url: dir_api + '/ventas/'+id+'/devoluciones',
 	                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 	            });
-	            
-            },
-            
-            /******************************
-            ADDPAGO
-            ******************************/
+	        },
+            //ADDPAGO *********************************************************
             addPago:function (pago, idVenta) {
 	            return $http({
 	            	method: 'POST',
@@ -88,11 +69,7 @@ app.service('ventasService', ['$http', function ($http) {
 	                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 	            })
             },
-            
-            
-            /******************************
-            DELETEPAGO
-            ******************************/
+            //DELETEPAGO *********************************************************
             deletePago:function (idPago) {
 	            return $http({
 	            	method: 'DELETE',
@@ -101,10 +78,7 @@ app.service('ventasService', ['$http', function ($http) {
 	                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 	            })
             },
-            
-            /******************************
-            ACTUALIZARNOTA
-            ******************************/
+            //ACTUALIZARNOTA *********************************************************
             actualizarNota:function (n, idVenta) {
 	            return $http({
 	            	method: 'POST',
@@ -113,12 +87,8 @@ app.service('ventasService', ['$http', function ($http) {
 	                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 	            })
             }
-            
-           
         }
 }]);
-
-
 
 
 app.service('notasService', ['$http', function ($http) {
@@ -131,43 +101,7 @@ app.service('notasService', ['$http', function ($http) {
 	                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 	            })
             },
-            
-            /******************************
-            ADDNOTA
-            ******************************/
-            addNota:function (nota) {
-	            return $http({
-	            	method: 'POST',
-	            	url: dir_api + '/notas/create',
-	            	data: $.param(nota),
-	                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-	            })
-            }
-            
-            
-            
-            
-           
-        }
-}]);
-
-
-
-
-app.service('notasService', ['$http', function ($http) {
-        return {
-            notas:function(d, h) {
-	           return $http({
-	            	method: 'POST',
-	            	url: dir_api + '/notas/index',
-	            	data: $.param({desde:d,hasta:h}),
-	                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-	            })
-            },
-            
-            /******************************
-            ADDNOTA
-            ******************************/
+            //ADDNOTA
             addNota:function (nota) {
 	            return $http({
 	            	method: 'POST',
@@ -176,24 +110,13 @@ app.service('notasService', ['$http', function ($http) {
 	                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 	            })
             },
-            
-                        
-            /******************************
-            DELETENOTA
-            ******************************/
+            //DELETENOTA
             deleteNota:function (id) { 
-	                    
 	            return $http({
 	            	method: 'DELETE',
 	            	url: dir_api + '/notas/'+id+'/delete',
 	                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 	            });
-	            
-            }
-            
-           
+	        }
         }
 }]);
-
-
-
